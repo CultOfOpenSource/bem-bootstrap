ddb28981c8dcac6af3496d7525f2e37653250802
Ilya Rogov <ilyar.software@gmail.com>
2017-12-05 17:51:59 +0200

    Rename to Alert page

--- bundles/alert/alert.bemjson.js
+++ bundles/alert/alert.bemjson.js
@@ -5,9 +5,9 @@ module.exports = {
     head: [
         { elem: 'meta', attrs: { name: 'description', content: '' } },
         { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-        { elem: 'css', url: 'index.min.css' }
+        { elem: 'css', url: 'alert.min.css' }
     ],
-    scripts: [{ elem: 'js', url: 'index.min.js' }],
+    scripts: [{ elem: 'js', url: 'alert.min.js' }],
     content: [
         {
             block: 'container',
