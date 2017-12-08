912b8ba392dfe383d1dd843b9d0038c47cf7a899
Ilya Rogov <ilyar.software@gmail.com>
2017-12-08 18:38:12 +0200

    Add tech tmpl-specs for testing templates

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -11,6 +11,41 @@ const levels = [
 
 module.exports = function (config) {
 
+    config.includeConfig('enb-bem-tmpl-specs');
+    var examples = config.module('enb-bem-tmpl-specs') // Создаём конфигуратор сетов
+        .createConfigurator('tmpl-specs', {            // в рамках таска `specs`.
+            coverage: {                                // Определяем общие опции для всех уровней-сетов.
+                engines: ['bemhtml'],
+                reportDirectory: 'coverage',
+                exclude: [
+                    '**/node_modules/**'
+                ],
+                reporters: [
+                    'text',
+                    'lcov'
+                ]
+            }
+        });
+
+    examples.configure({
+        destPath: 'components.tmpl-specs',
+        levels: levels,
+        engines: {
+            'bemhtml': {
+                tech: 'enb-bemxjst/techs/bemhtml',
+                options: {
+                    sourceSuffixes: ['bemhtml', 'bemhtml.js'],
+                    forceBaseTemplates: false,
+                    engineOptions: {
+                        naming: naming,
+                        escapeContent: false,
+                        elemJsInstances: false
+                    }
+                }
+            }
+        }
+    });
+
     config.nodes('bundles/*', function (nodeConfig) {
         nodeConfig.addTechs(nodeConfigSetup(levels, isProd, naming));
 
