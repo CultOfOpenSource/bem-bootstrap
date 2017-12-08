d2bb25a54a38d923e58563e7633cc29e42d6d2b1
Ilya Rogov <ilyar.software@gmail.com>
2017-12-08 11:21:56 +0200

    #6 Create template for Breadcrumb

--- components/breadcrumb/breadcrumb.bemhtml.js
+++ components/breadcrumb/breadcrumb.bemhtml.js
@@ -1,7 +1,38 @@
 block('breadcrumb')(
-    tag()('nav'),
-    attrs()({
-        'aria-label' : 'breadcrumb',
-        'role' : 'navigation'
+    tag()('ol'),
+    wrap()(function () {
+        return {
+            tag: 'nav',
+            attrs: {
+                'aria-label': 'breadcrumb',
+                'role': 'navigation'
+            },
+            content: this.ctx
+        };
     })
+);
+block('breadcrumb').elem('item')(
+    tag()('li'),
+    match(function () {
+        return this.ctx.url;
+    })(
+        content()(function () {
+            var ctx = this.ctx;
+            return {
+                tag: 'a',
+                attrs: {
+                    href: ctx.url
+                },
+                content: ctx.content
+            }
+        })
+    ),
+    match(function () {
+        return !this.ctx.url;
+    })(
+        mix()('active'),
+        attrs()({
+            'aria-current': 'page'
+        })
+    )
 );
---
+++ components/breadcrumb/breadcrumb.tmpl-specs/default.bemjson.js
@@ -0,0 +1,19 @@
+({
+    block: 'breadcrumb',
+    content: [
+        {
+            elem: 'item',
+            url: '#',
+            content: 'Foo'
+        },
+        {
+            elem: 'item',
+            url: '#',
+            content: 'Bar'
+        },
+        {
+            elem: 'item',
+            content: 'Baz'
+        }
+    ]
+});
